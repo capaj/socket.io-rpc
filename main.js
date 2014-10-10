a037b3a5a241fdae3328688e68be79fe924e793c
Jiri Spac <jiri.spac@ys.cz>
2014-10-10 02:51:27 +0200

    all of the needed variable now enclosed so that we can connect to more than one backend, bumped to 0.6.1

--- main.js
+++ main.js
@@ -1,214 +1,214 @@
 var Promise = require('bluebird');
 var _ = require('lodash');
 
-var runDate = new Date();
-var io;
-
-
-//channel template support vars
-var channelTemplates = {};
-var channelTemplatesSize = 0;
-var clientKnownChannels = {};
-
-var options = { channelTemplates: true };        //object of options which is fed input on createServer method call
-var deferreds = [];
-var serverChannels = {};
-var clientChannels = {};
-var getClientChannel = function (id, name) {
-    if (!clientChannels.hasOwnProperty(id)) {
-        //console.log("creating an object for client channels for ID " + id);
-		clientChannels[id] = {};
-    }
-    if (!clientChannels[id].hasOwnProperty(name)) {
-		//console.log("creating a client channel for client with ID " + id + " and channel name " + name);
-		clientChannels[id][name] = {};
-    }
-    return clientChannels[id][name];
-};
-
-var getChannelNames = function () {
-    var names = [];
-    for(var channel in serverChannels){
-        names.push(channel);
-    }
-    return names;
-};
-
-
-/**
- * @param {String} name
- * @param {Object} toExpose
- * @param {Function} [authFn] should return boolean value, if true access to a channel is allowed
- * @returns {RpcChannel}
- * @constructor
- */
-var RpcChannel = function (name, toExpose, authFn) {      //
-    this.fns = toExpose;
-
-    /**
-     * @type {Array<String>}
-     */
-    this.fnNames = [];
-
-    for (var fnName in toExpose) {
-        this.fnNames.push(fnName);
-    }
-    Object.freeze(toExpose);    //we won't support adding new methods to a channel after creation
-
-    if (options.channelTemplates) {
-
-        for (var tplId in channelTemplates) {
-            if (_.isEqual(channelTemplates[tplId], this.fnNames)) {
-                this.tplId = Number(tplId);   // converting string key to number
-            }
-        }
-        if (!_.isNumber(this.tplId)) {   //if no template matches the methods collection we save this channel methods as new template
-            var index = channelTemplatesSize + 1;
-            channelTemplates[index] = this.fnNames;
-            channelTemplatesSize = index;
-            this.tplId = index;
-        }
-
-    }
-    if (authFn) {
-        this.authFn = authFn;
-    }
-    this.authenticated = {};
-    this._socket = io.of('/rpc-' + name);
-    var self = this;
-    this._socket.on('connection', function (socket) {
-        var invocationRes = function (data) {
-            if (toExpose.hasOwnProperty(data.fnName) && typeof toExpose[data.fnName] === 'function') {
-                var retVal;
-                try{
-                    retVal = toExpose[data.fnName].apply(socket, data.args);
-                }catch(e){
-                    console.error('RPC method ' + data.fnName + ' on channel ' + name + ': ',e);
-                    retVal = e;
-                }
-                /* NOTE: Will return true for *any thenable object*, and isn't truly safe, since it will access the `then` property*/
-                if (retVal && typeof retVal.then === 'function') {    // this is async function, so 'return' is emitted after it finishes
-                    retVal.then(function (asyncRetVal) {
-                        socket.emit('resolve', { Id: data.Id, value: asyncRetVal });
-                    }, function (error) {
-                        if (error instanceof Error) {
-                            error = error.toString();
-                        }
-                        socket.emit('reject', { Id: data.Id, reason: error });
-
-                    });
-                } else {
-                    //synchronous
-                    if (retVal instanceof Error) {
-                        socket.emit('reject', { Id: data.Id, reason: retVal.toString() });
-                    } else {
-                        socket.emit('resolve', { Id: data.Id, value: retVal });
-                    }
-                }
-
-            } else {
-                socket.emit('reject', {Id: data.Id, reason: 'no such function has been exposed: ' + data.fnName });
-            }
-        };
-
-        if (authFn) {
-            if (self.authenticated[socket.id]) {
-                socket.on('call', invocationRes);
-                socket.on('disconnect', function () {
-                    delete self.authenticated[socket.id];   // cleaning up
-                });
-            } else {
-                socket.emit('connectFailed', "Authentication for channel failed");
-                socket.disconnect();    // forcibly disconnect
-            }
-        } else {
-            socket.on('call', invocationRes);
-        }
-
-    });
-
-    return this;
-};
-
-var invocationCounter = 0;
-var endCounter = 0;
-var callToClientEnded = function (Id) {
-    if (deferreds[Id]) {
-        delete deferreds[Id];
-        endCounter++;
-        if (endCounter == invocationCounter) {
-            invocationCounter = 0;
-            endCounter = 0;
-        }
-    } else {
-        console.warn("Deferred Id " + Id + " was resolved/rejected more than once, this should not occur.");
-    }
-};
-
-var rpcInstance = {
-	/**
-	 * @param {String} name
-	 * @returns {undefined|socket}
-	 */
-	getSocketFor: function(name) {
-		if (serverChannels[name]) {
-			return serverChannels[name]._socket;
-		}
-	},
-	/**
-	 *  Makes a channel available for clients
-	 * @param {String} name
-	 * @param {Object} toExpose
-	 * @param {Function} [authFn] when provided, channel will call it on any new connected client
-	 * @returns {rpcInstance}
-	 */
-	expose: function (name, toExpose, authFn) {
-		if (serverChannels[name]) {
-			console.warn("This channel name(" + name + ") is already exposed-ignoring the command.");
-		} else {
-			var channel = new RpcChannel(name, toExpose, authFn);
-			if (toExpose._socket) {
-				throw new Error('Failed to expose channel, _socket property is reserved for socket namespace');
-			}
-			serverChannels[name] = channel;
-		}
-		return rpcInstance;
-	},
-	/**
-	 *
-	 * @param {Socket} socket
-	 * @param {String} name
-	 * @returns {Promise} which will get resolved when client channel is ready
-	 */
-	loadClientChannel: function (socket, name) {
-		var channel = getClientChannel(socket.id, name);
-
-		/**
-		 * @type {Promise}
-		 */
-		if (!channel.dfd) {
-			channel.dfd = Promise.defer();
-
-			socket.on('disconnect', function onDisconnect() {
-				var err = function () {
-					throw new Error('Client channel disconnected, this channel is not available anymore')
-				};
-				for (var method in channel.fns) {
-					channel.fns[method] = err;	// references to client channel might be hold in client code, so we need to invalidate them
-				}
-				//console.log("disconnected clc " + name);
-			});
-		}
-		return channel.dfd.promise;
-	}
-};
-
 /**
  * @param {Manager} ioP socket.io manager instance returned by require('socket.io').listen(server);
  * @param {Express|Object} [opts] either an object which will extend default options or express app
- * @returns {Emitter|*}
+ * @returns {{getSocketFor: Function, expose: Function, loadClientChannel: Function}} rpc backend instance
  */
 module.exports = function createServer(ioP, opts) {
+
+	var runDate = new Date();
+	var io;
+
+	//channel template support hash, will store the templates
+	var channelTemplates = {};
+	var channelTemplatesSize = 0;
+	var clientKnownChannels = {};
+
+	var options = { channelTemplates: true };        //object of options which is fed input on createServer method call
+	var deferreds = [];
+	var serverChannels = {};
+	var clientChannels = {};
+	var getClientChannel = function (id, name) {
+		if (!clientChannels.hasOwnProperty(id)) {
+			//console.log("creating an object for client channels for ID " + id);
+			clientChannels[id] = {};
+		}
+		if (!clientChannels[id].hasOwnProperty(name)) {
+			//console.log("creating a client channel for client with ID " + id + " and channel name " + name);
+			clientChannels[id][name] = {};
+		}
+		return clientChannels[id][name];
+	};
+
+	var getChannelNames = function () {
+		var names = [];
+		for(var channel in serverChannels){
+			names.push(channel);
+		}
+		return names;
+	};
+
+
+	/**
+	 * @param {String} name
+	 * @param {Object} toExpose
+	 * @param {Function} [authFn] should return boolean value, if true access to a channel is allowed
+	 * @returns {RpcChannel}
+	 * @constructor
+	 */
+	var RpcChannel = function (name, toExpose, authFn) {      //
+		this.fns = toExpose;
+
+		/**
+		 * @type {Array<String>}
+		 */
+		this.fnNames = [];
+
+		for (var fnName in toExpose) {
+			this.fnNames.push(fnName);
+		}
+		Object.freeze(toExpose);    //we won't support adding new methods to a channel after creation
+
+		if (options.channelTemplates) {
+
+			for (var tplId in channelTemplates) {
+				if (_.isEqual(channelTemplates[tplId], this.fnNames)) {
+					this.tplId = Number(tplId);   // converting string key to number
+				}
+			}
+			if (!_.isNumber(this.tplId)) {   //if no template matches the methods collection we save this channel methods as new template
+				var index = channelTemplatesSize + 1;
+				channelTemplates[index] = this.fnNames;
+				channelTemplatesSize = index;
+				this.tplId = index;
+			}
+
+		}
+		if (authFn) {
+			this.authFn = authFn;
+		}
+		this.authenticated = {};
+		this._socket = io.of('/rpc-' + name);
+		var self = this;
+		this._socket.on('connection', function (socket) {
+			var invocationRes = function (data) {
+				if (toExpose.hasOwnProperty(data.fnName) && typeof toExpose[data.fnName] === 'function') {
+					var retVal;
+					try{
+						retVal = toExpose[data.fnName].apply(socket, data.args);
+					}catch(e){
+						console.error('RPC method ' + data.fnName + ' on channel ' + name + ': ',e);
+						retVal = e;
+					}
+					/* NOTE: Will return true for *any thenable object*, and isn't truly safe, since it will access the `then` property*/
+					if (retVal && typeof retVal.then === 'function') {    // this is async function, so 'return' is emitted after it finishes
+						retVal.then(function (asyncRetVal) {
+							socket.emit('resolve', { Id: data.Id, value: asyncRetVal });
+						}, function (error) {
+							if (error instanceof Error) {
+								error = error.toString();
+							}
+							socket.emit('reject', { Id: data.Id, reason: error });
+
+						});
+					} else {
+						//synchronous
+						if (retVal instanceof Error) {
+							socket.emit('reject', { Id: data.Id, reason: retVal.toString() });
+						} else {
+							socket.emit('resolve', { Id: data.Id, value: retVal });
+						}
+					}
+
+				} else {
+					socket.emit('reject', {Id: data.Id, reason: 'no such function has been exposed: ' + data.fnName });
+				}
+			};
+
+			if (authFn) {
+				if (self.authenticated[socket.id]) {
+					socket.on('call', invocationRes);
+					socket.on('disconnect', function () {
+						delete self.authenticated[socket.id];   // cleaning up
+					});
+				} else {
+					socket.emit('connectFailed', "Authentication for channel failed");
+					socket.disconnect();    // forcibly disconnect
+				}
+			} else {
+				socket.on('call', invocationRes);
+			}
+
+		});
+
+		return this;
+	};
+
+	var invocationCounter = 0;
+	var endCounter = 0;
+	var callToClientEnded = function (Id) {
+		if (deferreds[Id]) {
+			delete deferreds[Id];
+			endCounter++;
+			if (endCounter == invocationCounter) {
+				invocationCounter = 0;
+				endCounter = 0;
+			}
+		} else {
+			console.warn("Deferred Id " + Id + " was resolved/rejected more than once, this should not occur.");
+		}
+	};
+
+	var rpcInstance = {
+		/**
+		 * @param {String} name
+		 * @returns {undefined|socket}
+		 */
+		getSocketFor: function(name) {
+			if (serverChannels[name]) {
+				return serverChannels[name]._socket;
+			}
+		},
+		/**
+		 *  Makes a channel available for clients
+		 * @param {String} name
+		 * @param {Object} toExpose
+		 * @param {Function} [authFn] when provided, channel will call it on any new connected client
+		 * @returns {rpcInstance}
+		 */
+		expose: function (name, toExpose, authFn) {
+			if (serverChannels[name]) {
+				console.warn("This channel name(" + name + ") is already exposed-ignoring the command.");
+			} else {
+				var channel = new RpcChannel(name, toExpose, authFn);
+				if (toExpose._socket) {
+					throw new Error('Failed to expose channel, _socket property is reserved for socket namespace');
+				}
+				serverChannels[name] = channel;
+			}
+			return rpcInstance;
+		},
+		/**
+		 *
+		 * @param {Socket} socket
+		 * @param {String} name
+		 * @returns {Promise} which will get resolved when client channel is ready
+		 */
+		loadClientChannel: function (socket, name) {
+			var channel = getClientChannel(socket.id, name);
+
+			/**
+			 * @type {Promise}
+			 */
+			if (!channel.dfd) {
+				channel.dfd = Promise.defer();
+
+				socket.on('disconnect', function onDisconnect() {
+					var err = function () {
+						throw new Error('Client channel disconnected, this channel is not available anymore')
+					};
+					for (var method in channel.fns) {
+						channel.fns[method] = err;	// references to client channel might be hold in client code, so we need to invalidate them
+					}
+					//console.log("disconnected clc " + name);
+				});
+			}
+			return channel.dfd.promise;
+		}
+	};
+
 
 	if (opts) {
 		var app = opts.expressApp || opts;
