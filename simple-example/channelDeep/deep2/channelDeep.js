f8575b72de957c6a087323a7a391d492b67ed76b
Jiri Spac <capajj@gmail.com>
2015-01-20 20:52:38 +0000

    updated dependencies, test now cleans up the node server process, bumped version

--- simple-example/channelDeep/deep2/channelDeep.js
+++ simple-example/channelDeep/deep2/channelDeep.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird');
 module.exports = {
   purpose: function() {
     var df = Promise.defer();
-    //throw new Error('e'); //this throw would be catched and propagated to the client
+    //throw new Error('e'); //this throw is catched and propagated to the client
     setTimeout(function(){
       //throw new Error('e'); //this throw would NOT catched and crashes the process
       df.resolve(42);
--- test/basic.js
+++ test/basic.js
@@ -2,14 +2,15 @@ require('chai').should();
 var rpcClient = require('socket.io-rpc-client');
 var cp = require('child_process');
 
-var n = cp.fork('./simple-example/server.js');
+var server = cp.fork('./simple-example/server.js');
 
 var backend = rpcClient('http://localhost:8031');
 
 describe("simple remote channel",function(){
 
 	var channel;
-	beforeEach(function(done) {
+	var otherChannel;
+	before(function(done) {
 
 		backend.loadChannel('./rpc_channel_test')
 			.then(function(chnl) {
@@ -37,5 +38,26 @@ describe("simple remote channel",function(){
 		});
 	});
 
+	before(function(done) {
+		backend.loadChannel('./channelDeep/deep2/channelDeep')
+			.then(function(chnl) {
+				otherChannel = chnl;
+				done();
+				//channel.nonExistentRemoteFn();  // uncomment for typerror to be thrown and handled by otherwise promise method
+			}, function(err) {
+				throw err;
+			});
+	});
+
+	it('should yield a number when called', function(done){
+	    otherChannel.purpose().then(function(num) {
+			num.should.equal(42);
+			done();
+		});
+	});
+
+	after(function() {
+		server.kill();
+	})
 
 });
